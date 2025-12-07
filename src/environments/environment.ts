e56504cf20cfe26aadc1c8aec2fb53bca5d26c0f
KASSEABDOU <a.k21@zig.univ.sn>
2025-12-07 07:38:28 +0000

    commit 5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'apiUrl:https://gestiondaaras-2.onrender.com/api',
+  apiUrl: 'https://gestiondaaras-2.onrender.com/api',
   appName: 'Daara Management System (Dev)'
 };
